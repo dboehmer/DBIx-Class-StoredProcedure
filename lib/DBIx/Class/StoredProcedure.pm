7bec30a25c29e9b66f07c81502d0b5356ec816d2
Daniel Böhmer <post@daniel-boehmer.de>
2016-03-22 13:33:21 +0100

    Add inheritance to StoredProcedure base class and example

--- lib/DBIx/Class/StoredProcedure.pm
+++ lib/DBIx/Class/StoredProcedure.pm
@@ -1,3 +1,22 @@
 package DBIx::Class::StoredProcedure;
 
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+sub add_argument { shift->add_arguments(@_) }
+
+sub add_arguments {
+    my ( $class, @args ) = @_;
+
+    for my $arg (@args) { }
+}
+
+sub procedure {
+    my ( $class, $procedure ) = @_;
+
+    $class->table($procedure);    # necessary to satisfy DBIC
+}
+
 1;
--- t/lib/MySchema/StoredProcedure/Time.pm
+++ t/lib/MySchema/StoredProcedure/Time.pm
@@ -3,4 +3,16 @@ package MySchema::StoredProcedure::Time;
 use strict;
 use warnings;
 
+use base 'DBIx::Class::StoredProcedure';
+
+__PACKAGE__->procedure("time");
+
+__PACKAGE__->add_arguments(
+    offset => { data_type => "int", is_nullable => 1 },
+);
+
+__PACKAGE__->add_columns(
+    time => { data_type => "int", is_nullable => 0 },
+);
+
 1;
