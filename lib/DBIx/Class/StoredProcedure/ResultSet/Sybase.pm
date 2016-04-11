3f518a5f65644c31a73206e264bb27628bde88eb
Daniel Böhmer <post@daniel-boehmer.de>
2016-04-12 00:20:23 +0200

    WIP

--- lib/DBIx/Class/StoredProcedure/ResultSet/Sybase.pm
+++ lib/DBIx/Class/StoredProcedure/ResultSet/Sybase.pm
@@ -15,21 +15,53 @@ sub storage {
     my $stored_procedure = $self->result_source->stored_procedure;
     my $params           = $self->result_source->parameters_hashref;
 
+    my %out_params;
+    my @values;
+
     while ( my ( $name => $param ) = each %$params ) {
         my $type = uc( $param->{type} // 'IN' );
 
-        if ( $type eq 'IN' ) {
-            next;
+        my ( $variable, $value );
+
+        if ( $type eq 'IN' or $type eq 'INOUT' ) {
+            if ( exists $values->{$name} ) {
+                $value = \$values->{$name};
+            }
         }
-        elsif ( $type eq 'INOUT' or $type eq 'OUT' ) {
-            # TODO
+
+        if ( $type eq 'INOUT' or $type eq 'OUT' ) {
+            $variable = $name;
         }
         else {
             die "Invalid parameter type for parameter '$name'";
         }
     }
 
-    # TODO generate $sql
+    my $sql = "";
+
+    if (%out_params) {
+        $sql .= "DECLARE ";
+
+        while ( my ( $name => $param ) = each %$params ) {
+            my $type = uc( $param->{type} // 'IN' );
+
+            if ( $type eq 'INOUT' ) {
+                $sql .= "\@$name";
+                if ( exists $values->{$name} ) {
+                    my $value = $values->{name};
+                    if ( not defined $value ) {
+                        $value = "undef";
+                    }
+                    elsif ( not looks_like_number($value) ) {
+                        $value = qq('$value');
+                    }
+
+                    $sql .= " = $value";
+                }
+            }
+        }
+    }
+    $sql .= "EXECUTE $stored_procedure";
 
     return DBIx::Class::StoredProcedure::Storage->new(
         storage => $self->result_source->storage,
